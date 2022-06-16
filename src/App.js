00861846245a15bdb22b685d40fb0453c598f019
shoegazerr <82777389+shoegazerr@users.noreply.github.com>
2022-06-17 02:35:10 +0300

    modified:   src/App.js modified:   src/components/Header.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import {Flex, VStack, Heading, Spacer} from "@chakra-ui/layout"
 import { IconButton } from "@chakra-ui/react";
-import {FaInstagram, FaGithub, FaSun, FaMoon, FaVk} from 'react-icons/fa'
+import {FaInstagram, FaGithub, FaSun, FaMoon, FaVk, FaBandcamp, FaTelegram} from 'react-icons/fa'
 import {useColorMode} from '@chakra-ui/color-mode'
 import Header from './components/Header'
 import Social from './components/Social'
 import Profile from './components/Profile'
+import { Image } from '@chakra-ui/react'
+import { useMediaQuery } from '@chakra-ui/media-query'
 
 function App() {
 
 const {colorMode, toggleColorMode} = useColorMode();
 const isDark = colorMode === "dark";
+const {isNotSmallerScreen} = useMediaQuery("(min-wigth:600px)");
 
   return (
     <VStack p={5}>
@@ -19,7 +22,7 @@ const isDark = colorMode === "dark";
         <Spacer></Spacer>
         <IconButton ml={2} icon={<FaGithub/>} isRound="true" onClick={toggleColorMode}></IconButton>
         <IconButton ml={2} icon={<FaVk/>} isRound="true" onClick={toggleColorMode}></IconButton>
-        <IconButton ml={2} icon={<FaInstagram/>} isRound="true" onClick={toggleColorMode}></IconButton>
+        <IconButton ml={2} icon={<FaTelegram/>} isRound="true" onClick={toggleColorMode}></IconButton>
         <IconButton ml={8} icon={isDark ? <FaSun/>: <FaMoon />} isRound="true" onClick={toggleColorMode}></IconButton>
       </Flex>
       <Header></Header>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,29 @@ import React from 'react'
 import { useMediaQuery } from '@chakra-ui/media-query'
 import {useColorMode} from '@chakra-ui/color-mode'
 import {Stack, Flex, Text, Box} from '@chakra-ui/layout'
+import {Button} from '@chakra-ui/button'
+import { Image } from '@chakra-ui/react'
 
 function Header() {
 
   const{colorMode} = useColorMode();
   const isDark = colorMode === "dark";
 
-  const {isNotSmallerScreen} = useMediaQuery("(min-wigth:600px)");
+  /* const {isNotSmallerScreen} = useMediaQuery("(min-wigth:600px)"); */
 
   return (
     <Stack>
-      <Flex direction={isNotSmallerScreen ? "row" : "column"}
-             spacing ="200px" p={isNotSmallerScreen ? "32" : "0"}
-             alignSelf="flex-start">
-      <Box mt={isNotSmallerScreen ? "0" :16} align="flex-start">
-         <Text fontSize="5x1" fontWeight="semibold" >Hi, my name is</Text>
-         <Text fontSize="7x1" fontWeight="bold" bgGradient={isDark ? "linear(to-r, white, peachpuff, pink)" : "linear(to-r, darkblue, blue, cyan)"} bgClip="text">Shoegazer</Text>
+     <Flex>
+      <Box flexShrink={0}>
+         <Text fontSize="5xl" fontWeight="semibold" >Hi,</Text>
+         <Text fontSize="7xl" fontWeight="bold" bgGradient={isDark ? "linear(to-r, coral, peachpuff, pink)" : "linear(to-r, blue, darkturquoise, aquamarine)"} bgClip="text">I'm Shoegazer</Text>
+          <Button mt={8} colorScheme={isDark ? "red": "teal"} on click={() =>
+              window.open("")}>Contact me</Button>
       </Box>
-     
-
+      <Box flexShrink={0}>
+      <Image alignSelf='center'  ml="10" borderRadius="full" boxShadow='dark-lg'
+              boxSize="300px" src="https://i.pinimg.com/564x/57/fb/fe/57fbfe32e0f8a896d82c1141b8fcc9dc.jpg"/>
+      </Box>
 
       </Flex>
 
