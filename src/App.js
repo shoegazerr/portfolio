2984babeb68a58e53eb02650e7b85146f7368923
shoegazerr <82777389+shoegazerr@users.noreply.github.com>
2022-06-19 19:23:25 +0300

    modified:   package-lock.json modified:   package.json modified:   src/App.js modified:   src/components/Header.js src/components/Navigation.js src/components/logo.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {Flex, VStack, Heading, Spacer} from "@chakra-ui/layout"
 import { IconButton } from "@chakra-ui/react";
 import {FaInstagram, FaGithub, FaSun, FaMoon, FaVk, FaBandcamp, FaTelegram} from 'react-icons/fa'
+import { HamburgerIcon, ExternalLinkIcon } from '@chakra-ui/icons'
 import {useColorMode} from '@chakra-ui/color-mode'
 import Header from './components/Header'
+import { Link } from '@chakra-ui/react'
 import Social from './components/Social'
 import Profile from './components/Profile'
+import Navigation from './components/Navigation'
 import { Image } from '@chakra-ui/react'
 import { useMediaQuery } from '@chakra-ui/media-query'
+import {Menu, MenuButton, MenuList, MenuItem, MenuItemOption, MenuGroup, MenuOptionGroup, MenuDivider } from '@chakra-ui/react'
 
 function App() {
 
@@ -15,19 +19,8 @@ const isDark = colorMode === "dark";
 const {isNotSmallerScreen} = useMediaQuery("(min-wigth:600px)");
 
   return (
-    <VStack p={5}>
-      <Flex w="100%">
-        <Heading
-          ml="8" size="md" fontWeight="semibold" color={isDark ? "peachpuff": "blue"}>hello</Heading>
-        <Spacer></Spacer>
-        <IconButton ml={2} icon={<FaGithub/>} isRound="true" on click={() =>
-              window.open("https://github.com/shoegazerr")}></IconButton>
-        <IconButton ml={2} icon={<FaVk/>} isRound="true" on click={() =>
-              window.open("")}></IconButton>
-        <IconButton ml={2} icon={<FaTelegram/>} isRound="true" on click={() =>
-              window.open("")}></IconButton>
-        <IconButton ml={8} icon={isDark ? <FaSun/>: <FaMoon />} isRound="true" onClick={toggleColorMode}></IconButton>
-      </Flex>
+    <VStack>
+      <Navigation></Navigation>
       <Header></Header>
       <Social></Social>
       <Profile></Profile>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useMediaQuery } from '@chakra-ui/media-query'
 import {useColorMode} from '@chakra-ui/color-mode'
-import {Stack, Flex, Text, Box} from '@chakra-ui/layout'
+import {VStack, Flex, Text, Box} from '@chakra-ui/layout'
 import {Button} from '@chakra-ui/button'
 import { Image } from '@chakra-ui/react'
 
@@ -10,25 +10,21 @@ function Header() {
   const{colorMode} = useColorMode();
   const isDark = colorMode === "dark";
 
-  /* const {isNotSmallerScreen} = useMediaQuery("(min-wigth:600px)"); */
-
   return (
-    <Stack>
-     <Flex>
+    <VStack>
+     <Box flexShrink={0}>
+      <Image alignSelf='center' mt='10' ml="10" borderRadius="full" boxShadow='dark-lg'
+              boxSize="300px" src={isDark ? "https://i.pinimg.com/564x/57/fb/fe/57fbfe32e0f8a896d82c1141b8fcc9dc.jpg" : "https://i5.walmartimages.com/asr/8f716f20-98c6-46bd-a390-3863f0e0a4f3.8256601ad9b14c9dba39465cd7fa9bc2.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff"}/>
+      </Box>
+      <Flex>
       <Box flexShrink={0}>
          <Text fontSize="5xl" fontWeight="semibold" >Hi,</Text>
          <Text fontSize="7xl" fontWeight="bold" bgGradient={isDark ? "linear(to-r, coral, peachpuff, pink)" : "linear(to-r, blue, darkturquoise, aquamarine)"} bgClip="text">I'm Shoegazer</Text>
           <Button mt={8} colorScheme={isDark ? "red": "teal"} on click={() =>
-              window.open("")}>Contact me</Button>
+              window.open("")}>My Contacts</Button>
       </Box>
-      <Box flexShrink={0}>
-      <Image alignSelf='center'  ml="10" borderRadius="full" boxShadow='dark-lg'
-              boxSize="300px" src={isDark ? "https://i.pinimg.com/564x/57/fb/fe/57fbfe32e0f8a896d82c1141b8fcc9dc.jpg" : "https://i5.walmartimages.com/asr/8f716f20-98c6-46bd-a390-3863f0e0a4f3.8256601ad9b14c9dba39465cd7fa9bc2.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff"}/>
-      </Box>
-
       </Flex>
-
-    </Stack>
+    </VStack>
   )
 }
 
